1ef6587ed20ab1019d465fd8151bf829565e67a6
nourtaha09 <102453078+nourtaha09@users.noreply.github.com>
2022-04-17 08:32:49 +0300

    Add files via upload

---
+++ javascript_delcaring_function/3.3.js
@@ -0,0 +1,19 @@
+function percentageOfWorld1(population){
+    return `${(population/7900) * 100}%`
+}
+const china =  percentageOfWorld1(1400)
+const egypt = percentageOfWorld1(102)
+const uae =  percentageOfWorld1(10)
+console.log('china ',china)
+console.log('egypt ',egypt)
+console.log('uae ',uae)
+
+function percentageOfWorld2(population){
+    return `${(population/7900) * 100}%`
+}
+const china2 =  percentageOfWorld2(1400)
+const egypt2 = percentageOfWorld2(102)
+const uae2 =  percentageOfWorld2(10)
+console.log("china2 ",china2)
+console.log("egypt2 ",egypt2)
+console.log("uae2 ",uae2)
